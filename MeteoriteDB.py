93eab8ad401be2cd25b36c090082664169819086
mosikavrn <mosika17@gmail.com>
2017-08-31 20:29:18 +0300

    Rewrote a bit

--- MeteoriteDB.py
+++ MeteoriteDB.py
@@ -1,55 +1,113 @@
 import sqlite3
 import json
+import re
 
-fh = open("rows.json")
-mjson = json.load(fh)
+DBname = "meteorite.sqlite"
 
-conn = sqlite3.connect("meteorite.sqlite")
-cur = conn.cursor()
+def createDB():
+    conn = sqlite3.connect(DBname)
+    cur = conn.cursor()
+    #field duplicate: 1 - if there were landings with same latlon and name
+    #поле duplicate: 1 - если уже было падение с аналогичным именем и координатами
+    cur.execute('''CREATE TABLE IF NOT EXISTS mlandings
+                    (id INTEGER primary key UNIQUE,
+                     name TEXT,
+                     nametype TEXT,
+                     recclass TEXT,
+                     mass REAL,
+                     fall TEXT,
+                     year DATE,
+                     address_id INTEGER,
+                     duplicate INTEGER DEFAULT 0)''')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS mlandings
-                (id INTEGER primary key UNIQUE,
-                 name TEXT,
-                 nametype TEXT,
-                 recclass TEXT,
-                 mass REAL,
-                 fall TEXT,
-                 year DATE,
-                 address_id INTEGER)''')
+    cur.executescript('''CREATE TABLE IF NOT EXISTS address
+                    (id INTEGER primary key UNIQUE,
+                     reclat REAL,
+                     reclon REAL,
+                     geodata TEXT,
+                     country_id INTEGER);
+                     CREATE UNIQUE INDEX latlon ON address(reclat,reclon)''')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS address
-                (id INTEGER primary key UNIQUE,
-                 reclat REAL,
-                 reclon REAL,
-                 geodata TEXT,
-                 country_id INTEGER)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS country
+                    (id INTEGER primary key UNIQUE,
+                     country_longname TEXT UNIQUE,
+                     country_shortname TEXT UNIQUE,
+                     area REAL)''')
+    cur.close()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS country
-                (id INTEGER primary key UNIQUE,
-                 country_longname TEXT UNIQUE,
-                 country_shortname TEXT UNIQUE,
-                 aria REAL)''')
+#fill up database from file "rows.json" that was downloaded from NASA.gov
+def fillUpDB():
+    fh = open("rows.json")
+    mjson = json.load(fh)
 
-for mitem in (mjson['data']):
-    name = mitem[8]
-    if mitem[9] is not None : mid = int(mitem[9])
-    nametype = mitem[10]
-    recclass = mitem[11]
-    if mitem[12] is not None : mass = float(mitem[12])
-    fall = mitem[13]
-    year = mitem[14]
-    if mitem[15] is not None : reclat = float(mitem[15])
-    if mitem[16] is not None : reclon = float(mitem[16])
+    conn = sqlite3.connect(DBname)
+    cur = conn.cursor()
 
-    print(mid, name, nametype, recclass, mass, fall, year, reclat, reclon)
+    for mitem in (mjson['data']):
+        name = mitem[8]
+        mid = int(mitem[9]) if mitem[9] is not None else None
+        nametype = mitem[10]
+        recclass = mitem[11]
+        mass = float(mitem[12]) if mitem[12] is not None else None
+        fall = mitem[13]
+        year = mitem[14]
+        reclat = float(mitem[15]) if mitem[15] is not None else None
+        reclon = float(mitem[16]) if mitem[16] is not None else None
 
-    cur.execute('''INSERT OR IGNORE INTO address (reclat, reclon)
-        VALUES (?, ?)''', (reclat, reclon))
-    cur.execute('SELECT id from address where reclat = ? and reclon = ?', (reclat, reclon))
-    address_id = cur.fetchone()[0]
+        print(mid, name, nametype, recclass, mass, fall, year, reclat, reclon)
 
-    cur.execute('''INSERT OR IGNORE INTO mlandings (id, name, nametype, recclass, mass, fall, year, address_id)
-        VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )''', (mid, name, nametype, recclass, mass, fall, year, address_id))
+        #latlon stores in table address and then forein key inserted into mlandings
+        if reclat is None or reclon is None or (reclat == 0 and reclon == 0):
+            address_id = None
+        else:
+            cur.execute('''INSERT OR IGNORE INTO address (reclat, reclon)
+                VALUES (?, ?)''', (reclat, reclon))
+            cur.execute('SELECT id from address where reclat = ? and reclon = ?', (reclat, reclon))
+            address_id = cur.fetchone()[0]
 
-    if mid % 50 == 0 : conn.commit()
-cur.close()
+
+        cur.execute('''INSERT OR IGNORE INTO mlandings (id, name, nametype, recclass, mass, fall, year, address_id)
+            VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )''', (mid, name, nametype, recclass, mass, fall, year, address_id))
+
+        if mid % 50 == 0 : conn.commit()
+    cur.close()
+
+def cleanUpDB():
+    conn = sqlite3.connect(DBname)
+    maincur = conn.cursor()
+    cur = conn.cursor()
+
+    #find all landings with same latlon
+    maincur.execute('''select mlandings.id, mlandings.name, mlandings.address_id from
+                	(select count(1) mcount, address_id
+                    	from mlandings
+                    	where address_id is not null
+                    	group by address_id) msel
+                    join mlandings on msel.address_id = mlandings.address_id
+                    where msel.mcount > 1
+                    ''')
+
+    for item in maincur:
+        short_meteorite_name = re.findall('([a-zA-Z ]+)',item[1])[0].strip()
+        print(item[1])
+        mlanding_id = int(item[0])
+        address_id = item[2]
+
+        #for each landing find first one with same latlon
+        cur.execute('''select mlandings.id, mlandings.name from mlandings
+                    where address_id = ? and name LIKE ?
+                    ORDER BY mlandings.name LIMIT 1''',
+                    (address_id, '%'+short_meteorite_name+'%'))
+        firstlanding_id = int(cur.fetchone()[0])
+
+        #if current landing is not the first one, set duplicate parametr = 1
+        if firstlanding_id != mlanding_id:
+            cur.execute('''UPDATE mlandings SET duplicate = 1 where id = ?''', (mlanding_id,))
+            conn.commit()
+
+    cur.close()
+    maincur.close()
+
+createDB()
+fillUpDB()
+cleanUpDB()
--- MeteoriteLocation.py
+++ MeteoriteLocation.py
@@ -12,101 +12,104 @@ def defineCountry(lst):
             return (item['long_name'],item['short_name'])
     return ('','')
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+def defineLocation():
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
-#Enter your Google geocode key
-api_key =  config.GoogleKey
+    #Enter your Google geocode key
+    api_key =  config.GoogleKey
 
-serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
+    serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
 
-conn = sqlite3.connect('meteorite.sqlite')
-cur = conn.cursor()
+    conn = sqlite3.connect('meteorite.sqlite')
+    cur = conn.cursor()
 
-count = 0
-while True:
-    #seaching for the first unretrived address
-    cur.execute('''SELECT address.id,
-                          address.reclat,
-                          address.reclon,
-                          address.geodata,
-                          address.country_id
-        FROM address
-        JOIN mlandings ON mlandings.address_id = address.id
-        WHERE address.reclat IS NOT NULL and  address.reclon IS NOT NULL
-        AND address.reclat != 0 AND address.reclon != 0
-        AND (address.geodata NOT LIKE '%ZERO_RESULTS%' or address.geodata IS NULL)
-        AND address.country_id IS NULL
-        ORDER BY mlandings.year desc LIMIT 1''')
-    res = cur.fetchone()
-    address_id = res[0]
-    reclat = res[1]
-    reclon = res[2]
-    geodata = ''
-    if res[3] is not None:
-        geodata = res[3]
-    if res[4] is not None: country_id = res[4]
-    print(res)
+    count = 0
+    while True:
+        #seaching for the first unretrived address
+        cur.execute('''SELECT address.id,
+                              address.reclat,
+                              address.reclon,
+                              address.geodata,
+                              address.country_id
+            FROM address
+            JOIN mlandings ON mlandings.address_id = address.id
+            WHERE address.reclat IS NOT NULL and  address.reclon IS NOT NULL
+            AND address.reclat != 0 AND address.reclon != 0
+            AND (address.geodata NOT LIKE '%ZERO_RESULTS%' or address.geodata IS NULL)
+            AND address.country_id IS NULL
+            ORDER BY mlandings.year desc LIMIT 1''')
+        res = cur.fetchone()
+        address_id = res[0]
+        reclat = res[1]
+        reclon = res[2]
+        geodata = ''
+        if res[3] is not None:
+            geodata = res[3]
+        if res[4] is not None: country_id = res[4]
+        print(res)
 
-    if not geodata:
-        print(geodata)
-        #creating an url for the google api
-        url = serviceurl +'latlng='+ str(reclat)+','+str(reclon) + '&key=' + api_key
-        print(url)
+        if not geodata:
+            print(geodata)
+            #creating an url for the google api
+            url = serviceurl +'latlng='+ str(reclat)+','+str(reclon) + '&key=' + api_key
+            print(url)
 
-        #geting address from google
-        uh = urllib.request.urlopen(url, context=ctx)
-        data = uh.read().decode()
+            #geting address from google
+            uh = urllib.request.urlopen(url, context=ctx)
+            data = uh.read().decode()
 
-        try:
-            js = json.loads(data)
-        except:
-            print(data)  # We print in case unicode causes an error
+            try:
+                js = json.loads(data)
+            except:
+                print(data)  # We print in case unicode causes an error
+                continue
+
+            if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
+                print('==== Failure To Retrieve ====')
+                print(data)
+                break
+
+            #updating address in the DB
+            cur.execute('''UPDATE address SET geodata = ? WHERE id = ?''',
+                (memoryview(data.encode()), address_id ) )
+            conn.commit()
+        else:
+            try:
+                js = json.loads(geodata)
+            except:
+                print(geodata)  # We print in case unicode causes an error
+                continue
+
+        if js['status'] == 'ZERO_RESULTS':
+            conn.commit()
+            print('ZERO_RESULTS')
             continue
 
-        if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
-            print('==== Failure To Retrieve ====')
-            print(data)
+        country_names = defineCountry(js["results"][0]["address_components"])
+        print(country_names)
+
+        cur.execute('''INSERT OR IGNORE INTO country (country_longname, country_shortname)
+            VALUES (?, ?)''', country_names)
+        cur.execute('SELECT id from country where country_longname = ?', (country_names[0],))
+        country_id = cur.fetchone()[0]
+
+        cur.execute('''UPDATE address SET country_id = ? WHERE id = ?''',
+            (country_id, address_id ) )
+
+        conn.commit()
+        count = count + 1
+        if count % 10 == 0 :
+            print('Pausing for a bit...')
+            time.sleep(5)
+
+        if count == 1200:
+            print('Finite incantatem')
             break
 
-        #updating address in the DB
-        cur.execute('''UPDATE address SET geodata = ? WHERE id = ?''',
-            (memoryview(data.encode()), address_id ) )
-        conn.commit()
-    else:
-        try:
-            js = json.loads(geodata)
-        except:
-            print(geodata)  # We print in case unicode causes an error
-            continue
-
-    if js['status'] == 'ZERO_RESULTS':
-        conn.commit()
-        print('ZERO_RESULTS')
-        continue
-
-    country_names = defineCountry(js["results"][0]["address_components"])
-    print(country_names)
-
-    cur.execute('''INSERT OR IGNORE INTO country (country_longname, country_shortname)
-        VALUES (?, ?)''', country_names)
-    cur.execute('SELECT id from country where country_longname = ?', (country_names[0],))
-    country_id = cur.fetchone()[0]
-
-    cur.execute('''UPDATE address SET country_id = ? WHERE id = ?''',
-        (country_id, address_id ) )
-
     conn.commit()
-    count = count + 1
-    if count % 10 == 0 :
-        print('Pausing for a bit...')
-        time.sleep(5)
+    cur.close()
 
-    if count == 1200:
-        print('Finite incantatem')
-        break
-
-conn.commit()
-cur.close()
+defineLocation()
